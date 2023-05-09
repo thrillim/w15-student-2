b40b4ddffdc625c4179646e008f0412b1e6ab8a3
thrillim <hauniuni23@gmail.com>
2023-05-09 23:10:55 +0700

    delete with full function

--- app/components/table/page.tsx
+++ app/components/table/page.tsx
@@ -1,9 +1,10 @@
 'use client'
 import Student from "@prisma/client";
-import useSWR from 'swr'
+import useSWR, {mutate} from 'swr'
 import UpdateButton from '../updateButton/page'
 import CreateButton from '../createButton/page'
 import { Student } from '@prisma/client';
+import Table from './page';
 
 
 function rewriteDate(date: string) {
@@ -13,10 +14,19 @@ function rewriteDate(date: string) {
   return day + "-" + month + "-" + year;
 }
 
+function deleteStudent(id: string) {
+  const data = fetch(`/api/deleteStudent/${id}`, {
+    method: 'DELETE',
+    headers: new Headers({
+      'Content-Type': 'application/json',
+    })
+  })
+}
+
 const fetcher = () => fetch('/api/getStudents', {method: "GET"}).then((res) => res.json())
 
 export default function Table() {
-  const { data:students, isLoading } = useSWR<Student[]>('/api/getStudents', fetcher)
+  const { data:students, isLoading, mutate } = useSWR<Student[]>('/api/getStudents', fetcher)
   return (
     <div>        
       {isLoading && <div>Loading...</div>}
@@ -44,6 +54,10 @@ export default function Table() {
               <td>{rewriteDate(student.birthDate)}</td>
               <td>{student.hometown}</td>
               <td><UpdateButton id={student.id}/></td>
+              <td><button className="btn btn-error btn-sm" onClick={()=> {
+                deleteStudent(student.id);
+                mutate() // refetch swr
+              }}>Delete</button></td>
               {/* <td><DeleteBtn id={student.id} /></td> */}
             </tr>
           ))}
